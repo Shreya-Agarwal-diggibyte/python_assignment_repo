2220b58932039bd90b615dab1bd14a7fc1363224
Shreya Agarwal <shreyaagarwal.g@diggibyte.com>
2025-06-09 11:06:27 +0530

    floor_ceil_rint : added util and driver file for floor_ceil_rint

---
+++ src/floor_ceil_rint/driver.py
@@ -0,0 +1,3 @@
+from util import find_floor_ceil_rint
+
+find_floor_ceil_rint()
---
+++ src/floor_ceil_rint/util.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def find_floor_ceil_rint():
+
+    np.set_printoptions(legacy='1.13')
+
+    n=np.array(input().split(),float)
+
+    return(np.floor(n))
+    return(np.ceil(n))
+    return(np.rint(n))
+
+
